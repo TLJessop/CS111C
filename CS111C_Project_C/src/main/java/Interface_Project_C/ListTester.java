c76b4f3372c861c2cc38f5c3ae7e402930a83d7d
Thomas Jessop <tljessop@hotmail.com>
2019-02-26 04:13:39 -0800

    m

---
+++ CS111C_Project_C/src/main/java/Interface_Project_C/ListTester.java
@@ -0,0 +1,26 @@
+package Interface_Project_C;
+
+import java.util.Arrays;
+
+public class ListTester {
+
+    public static void main(String[] args){
+        ArrayHeadTailList<Integer> numList = new ArrayHeadTailList<>(4);
+//        numList.addFront(1);
+//        numList.addFront(2);
+//        numList.addFront(3);
+//        numList.addFront(4);
+//        numList.addFront(5);
+//        numList.addFront(6);
+//        numList.addFront(7);
+//        numList.addFront(8);
+        numList.addBack(1);
+        numList.addBack(2);
+        numList.addBack(3);
+        numList.addBack(4);
+        numList.addBack(5);
+
+
+
+    }
+}
