175d24c4c91993c4ddb0fe3434ddd0978c7ae81f
Thomas Jessop <tljessop@hotmail.com>
2019-03-11 15:15:30 -0700

    added selection sort method to the Sort class

---
+++ CS111C_Module8/src/main/java/VideoCodeAlong/Sorts.java
@@ -0,0 +1,38 @@
+package VideoCodeAlong;
+
+import java.util.List;
+
+public class Sorts {
+
+
+    public static <T extends Comparable<T>> void selectionSort(Comparable<T>[] items){
+        for (int i = 0; i < items.length; i++) {
+           int indenOfMin = getMinIndex(items, i);
+           Comparable temp = items[i];
+           items[i] = items[indenOfMin];
+           items[indenOfMin] = temp;
+        }
+    }
+
+    private static <T extends Comparable<T>> int getMinIndex(Comparable<T>[] items, int arrayPosition ){
+        Comparable minObject = items[arrayPosition];
+        int minIndex = arrayPosition;
+
+        for (int i = arrayPosition + 1; i < items.length; i++) {
+            if (minObject.compareTo(items[i]) < 0 ) {
+                minObject = items[i];
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+
+
+
+
+
+
+
+
+
+}//Sorts
