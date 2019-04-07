bd37049e65518e936efcf921d55264bcdd86c29e
Thomas Jessop <tljessop@hotmail.com>
2019-04-07 02:59:25 -0700

    add a basic dog class

---
+++ CS211s_Module11/src/main/java/VideoCodeAlong/Dog.java
@@ -0,0 +1,63 @@
+package VideoCodeAlong;
+
+public class Dog {
+
+    //Class variable
+    private static int countOfDogs;
+
+    //Instance variables
+    private String name, breed;
+    private int age;
+    private boolean vaccinated;
+
+    //Constructor
+
+    public Dog(String name, String breed, int age, boolean vaccinated){
+        this.age = age;
+        this.name = name;
+        this.breed = breed;
+        this.vaccinated = vaccinated;
+        countOfDogs++;
+    }
+
+    //Getters and Setters
+    public static int getCountOfDogs() {
+        return countOfDogs;
+    }
+
+    public static void setCountOfDogs(int countOfDogs) {
+        Dog.countOfDogs = countOfDogs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isVaccinated() {
+        return vaccinated;
+    }
+
+    public void setVaccinated(boolean vaccinated) {
+        this.vaccinated = vaccinated;
+    }
+}//Dog
---
+++ CS211s_Module11/src/main/java/VideoCodeAlong/Tester.java
@@ -0,0 +1,14 @@
+package VideoCodeAlong;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Tester {
+
+    public static void main(String[] args){
+        HashSet<String> stringSet = new HashSet<>();
+
+
+    }
+
+}
