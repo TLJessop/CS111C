4d8a310fc59b4414f9e7cc8202b3d21cf261ec50
Thomas Jessop <tljessop@hotmail.com>
2019-04-16 10:37:53 -0700

    Final turn version

--- CS111C_Module12/src/main/java/HomeWork/LinkedHeadTailList.java
+++ CS111C_Module12/src/main/java/HomeWork/LinkedHeadTailList.java
@@ -1,14 +1,17 @@
 package HomeWork;
 
+import java.util.Objects;
+
 public class LinkedHeadTailList<T extends Comparable<? super T>> implements HeadTailListInterface<T>, Comparable<LinkedHeadTailList<T>>{
 
     //Instance data
     private int numberOfEntries;
-    private Node headNode;
+    private Node headNode, tailNode;
 
     //Constructors
     public LinkedHeadTailList( T firstEntry){
         headNode = new Node(firstEntry, null, null);
+        tailNode = headNode;
         numberOfEntries = 1;
     }
 
@@ -17,11 +20,10 @@ public class LinkedHeadTailList<T extends Comparable<? super T>> implements Head
         numberOfEntries = 0;
     }
 
-
-
     public void addFront(T newEntry) {
         if (headNode == null){
             headNode = new Node(newEntry,null,null);
+            tailNode = headNode;
         } else {
             Node tempNode = headNode;
             headNode =  new Node(newEntry,null,null);
@@ -31,120 +33,99 @@ public class LinkedHeadTailList<T extends Comparable<? super T>> implements Head
         numberOfEntries++;
     }
 
-    /**
-     * Adds a new entry to the end of the list.
-     * Entries currently in the list are unaffected.
-     * The list's size is increased by 1.
-     *
-     * @param newEntry The object to be added as a new entry.
-     */
-    @Override
     public void addBack(T newEntry) {
 
-        if (headNode == null){
-            headNode = new Node(newEntry,null,null);
+        if (tailNode == null){
+            tailNode = new Node(newEntry,null,null);
+            headNode = tailNode;
         } else {
-            Node curentNode = headNode;
-            while (curentNode.next != null){
-                curentNode = curentNode.next;
-            }
-            System.out.println(curentNode.data);
-            curentNode.next = new Node(newEntry,null,curentNode);
+            Node tempNode = tailNode;
+            tailNode = new Node(newEntry,null, null);
+            tempNode.next = tailNode;
+            tailNode.before = tempNode;
         }
         numberOfEntries++;
     }
 
-    /**
-     * Removes an entry from the beginning of the list.
-     * Entries currently in the list are shifted up.
-     * The list's size is decreased by 1.
-     *
-     * @return A reference to the removed entry or null if the list is empty.
-     */
-    @Override
+
     public T removeFront() {
-        return null;
-    }
+        if (headNode != null){
+            Node tempNode = headNode;
 
-    /**
-     * Removes an entry from the end of the list.
-     * Entries currently in the list are unaffected.
-     * The list's size is decreased by 1.
-     *
-     * @return A reference to the removed entry or null if the list is empty.
-     */
-    @Override
-    public T removeBack() {
-        return null;
-    }
-
-    /**
-     * Removes all entries from this list.
-     */
-    @Override
-    public void clear() {
-
-    }
-
-    /**
-     * Retrieves the entry at a given position in this list.
-     *
-     * @param givenPosition An integer that indicates the position of the desired entry.
-     * @return A reference to the indicated entry or null if the index is out of bounds (less than zero greater than or equal to the size).
-     */
-    @Override
-    public T getEntry(int givenPosition) {
-        return null;
-    }
-
-    /**
-     * Displays the contents of the list to the console, in order.
-     */
-    @Override
-    public void display() {
-        System.out.print("LinkedHeadList { ");
-        Node curentNode = headNode;
-        while (curentNode != null){
-            System.out.print(curentNode.data + ", ");
-            curentNode = curentNode.next;
+            headNode = headNode.next;
+            if (headNode != null) {
+                headNode.before = null;
+            }
+            numberOfEntries--;
+            return tempNode.data;
+        } else {
+            return null;
         }
-        System.out.println("}");
+    }
+
+    public T removeBack() {
+
+        if (tailNode != null) {
+         Node tempNode = tailNode;
+         tailNode = tailNode.before;
+
+         if (tailNode != null ){
+             tailNode.next = null;
+
+         }
+         numberOfEntries--;
+         if (numberOfEntries == 0) this.clear();
+         return tempNode.data;
+        } else {
+            return null;
+        }
 
     }
 
-    /**
-     * Checks whether this list contains a given entry.
-     *
-     * @param anEntry the object to search for in the list.
-     * @return the position of the entry that was found or -1 if the object is not found.
-     */
-    @Override
+     public void clear() {
+        headNode = null;
+        tailNode = null;
+        numberOfEntries =0;
+    }
+
+    public T getEntry(int givenPosition) {
+        if (givenPosition < 0  || givenPosition > numberOfEntries -1 ){
+            return null;
+        } else {
+            Node currentNode = headNode;
+            for (int i = 0; i < givenPosition; i++) {
+
+                currentNode = currentNode.next;
+            }
+            return currentNode.data;
+        }
+    }
+
+    public void display() {
+        System.out.println(this);
+    }
+
     public int contains(T anEntry) {
-        return 0;
+        int count = 0;
+        Node currenNode = headNode;
+        while (currenNode != null){
+            if (currenNode.data.equals(anEntry)){
+                return count;
+            }
+            currenNode = currenNode.next;
+            count++;
+        }
+        return -1;
     }
 
-    /**
-     * Gets the length of this list.
-     *
-     * @return The integer number of entries currently in the list.
-     */
-    @Override
+
     public int size() {
         return numberOfEntries;
     }
 
-    /**
-     * Checks whether this list is empty.
-     *
-     * @return True if the list is empty, or false if the list contains one or more elements.
-     */
-
     public boolean isEmpty() {
-        return (headNode == null ) ? true : false;
+        return numberOfEntries == 0;
     }
-
-
-
 
 
 // Node class copied over from LList and added a leading reference
@@ -197,38 +178,77 @@ public class LinkedHeadTailList<T extends Comparable<? super T>> implements Head
 
 
 
+    //This will run O(n), but it makes sure to keep the hashCode / equals contract
     @Override
     public int hashCode() {
-        return super.hashCode();
+        int hash = Objects.hash(headNode);
+        Node current = headNode;
+        while (current != null){
+            hash += Objects.hash(current.data);
+        }
+        return hash;
     }
 
-
+    //This runs O(n), but it makes sure to check all items
     @Override
     public boolean equals(Object obj) {
-        return super.equals(obj);
+        if (obj == null ) return false;
+        if (obj == this ) return true;
+        if (obj instanceof  LinkedHeadTailList<?>){
+
+            LinkedHeadTailList<?> other = (LinkedHeadTailList<?>) obj;
+
+            if (other.numberOfEntries != this.numberOfEntries) return false;
+
+            Node currentNode =  headNode;
+            //I know this looks weird, but it makes typeeraser happy
+            Node otherNode = (Node) other.headNode;
+
+            while (currentNode != null){
+                if (!currentNode.data.equals(otherNode.data)){
+                    return false;
+                }
+                currentNode = currentNode.next;
+                otherNode = otherNode.next;
+            }
+
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
     @Override
     public String toString() {
-        return super.toString();
+        StringBuilder outString = new StringBuilder("[");
+        Node currentNode = headNode;
+        while (currentNode != null){
+            outString.append(currentNode.data);
+            if (currentNode != tailNode) outString.append(" ,");
+            currentNode = currentNode.next;
+        }
+        outString.append("]");
+        if (headNode != null && tailNode != null){
+            outString.append(" head =" + (headNode.data) + " tail=" + tailNode.data);
+        }
+        return outString.toString();
     }
 
     @Override
-    public int compareTo(LinkedHeadTailList<T> o) {
-        return 0;
+    public int compareTo(LinkedHeadTailList<T> other) {
+
+        Node currentNode = headNode;
+        Node otherNode = other.headNode;
+        while (currentNode != null && otherNode != null){
+            if (!currentNode.data.equals(otherNode.data)){
+                return currentNode.data.compareTo(otherNode.data);
+            }
+            currentNode = currentNode.next;
+            otherNode = otherNode.next;
+        }
+
+        return Integer.compare(this.numberOfEntries, other.numberOfEntries);
     }
 
-    public static void main(String[] args) {
-        LinkedHeadTailList<String> test = new LinkedHeadTailList<>();
-
-        test.addFront("one");
-        test.addFront("two");
-        test.addFront("three");
-        test.addFront("four");
-        test.addBack("Zore");
-
-        test.display();
-
-    }
 }//LinkedHeadTailList
