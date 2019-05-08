ff765a16355543198afac0aeecc78e7d49d33bec
Thomas Jessop <tljessop@hotmail.com>
2019-05-07 17:22:41 -0700

    Added very simple vertex class. This will have a few major updates

---
+++ CS111C_Module16/src/main/java/VideoCodealong/Vertex.java
@@ -0,0 +1,36 @@
+package VideoCodealong;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vertex <T> {
+
+    //Instance data
+    private T data;
+    private List<Edge<T>> edges;
+
+    //Constructor
+    protected Vertex(T data){
+        this.data = data;
+        this.edges = new ArrayList<Edge<T>>();
+    }
+
+    //Core methods
+    protected void setData(T data){
+        this.data = data;
+    }
+
+    protected T getData(){
+        return data;
+    }
+
+    protected void addEdge(Edge<T> newEdge){
+        edges.add(newEdge);
+    }
+
+    protected List<Edge<T>> getEdges(){
+        return new ArrayList<Edge<T>>(edges);
+    }
+
+
+}//Vertex
