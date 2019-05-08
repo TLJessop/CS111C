6eb2aebc24efb066e778a4a63248a461aecfdfa4
Thomas Jessop <tljessop@hotmail.com>
2019-05-07 19:25:23 -0700

    added the skeleton of my directed graph class

---
+++ CS111C_Module16/src/main/java/VideoCodealong/DirectedGraph.java
@@ -0,0 +1,52 @@
+package VideoCodealong;
+
+import java.util.Queue;
+import java.util.Stack;
+
+public class DirectedGraph<T> implements GraphInterface<T> {
+
+
+    public boolean addVertex(T vertexLabel) {
+        return false;
+    }
+
+    public boolean addEdge(T begin, T end, double edgeWeight) {
+        return false;
+    }
+
+    public boolean addEdge(T begin, T end) {
+        return false;
+    }
+
+    public boolean hasEdge(T begin, T end) {
+        return false;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int getNumberOfVertices() {
+        return 0;
+    }
+
+    public int getNumberOfEdges() {
+        return 0;
+    }
+
+    public void clear() {
+
+    }
+
+    public Queue<T> getBreadthFirstTraversal(T origin) {
+        return null;
+    }
+
+    public Queue<T> getDepthFirstTraversal(T origin) {
+        return null;
+    }
+
+    public Stack<T> getTopologicalOrder() {
+        return null;
+    }
+}//DirectedGraph
